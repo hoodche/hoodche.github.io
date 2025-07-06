b99bf90aadc004a16b8b67b52618076ebdf15fbd
nateto <nateto@hotmail.com>
2025-07-06 17:04:50 +0200

    mas

--- js/main.js
+++ js/main.js
@@ -22,8 +22,6 @@ async function pedirContrasena() {
   const inputHash = await digestMessage(input.toUpperCase());
 
   if (inputHash === contrasenaCorrectaHash) {
-    // Set your YouTube embed URL here
-    youtubeVideo.src="https://www.youtube.com/embed/HFojlF1wS2o?si=aH69hdotP5xZ5-va";
     videoSecreto.style.display = "block";
   } else {
     errorElement.textContent = "Contraseña incorrecta. Inténtalo de nuevo";
